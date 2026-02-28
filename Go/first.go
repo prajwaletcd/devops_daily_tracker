e9d226ff8446fe2420b39c361ce0ee6a9125c70f
Prajwal Shetty <prajwalshettyr09@gmail.com>
2026-02-28 14:16:31 +0530

    feat:working on basics of glow wrt var/conditional blocks

--- Go/first.go
+++ Go/first.go
@@ -4,4 +4,8 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hey first day of GoLang")
+	var name string = "Prajwal Shetty"
+	age := 28
+
+	fmt.Println("Hey my name is :", name, "and i'm", age, "year young DevOps Enthusiast!")
 }
