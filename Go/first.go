d58f8f934eb37f0cff1e53402df0699e60ab759f
Prajwal Shetty <prajwalshettyr09@gmail.com>
2026-03-01 20:42:02 +0530

    feat: feature1 into the repo

---
+++ Go/first.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hey first day of GoLang")
+	var name string = "Prajwal Shetty"
+	age := 28
+
+	fmt.Println("Hey my name is :", name, "and i'm", age, "year young DevOps Enthusiast!")
+}
